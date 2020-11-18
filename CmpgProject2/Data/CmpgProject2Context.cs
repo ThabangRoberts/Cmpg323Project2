aa0c47254125b3618802bf835f36d217b5a71dcf
Thabang Roberts <thabangRoberts.work@gmail.com>
2020-11-18 08:07:44 +0200

    Finalising

---
+++ CmpgProject2/Data/CmpgProject2Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CmpgProject2.Data
+{
+    public class CmpgProject2Context : DbContext
+    {
+        public CmpgProject2Context (DbContextOptions<CmpgProject2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CmpgProject2.Data.OrganizationX> OrganizationX { get; set; }
+    }
+}
